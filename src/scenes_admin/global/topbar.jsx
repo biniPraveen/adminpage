5cb31c94eccc295f4180ea064b0e071906702225
Bini Babu Shini <binibabu9590@gmail.com>
2023-01-31 00:19:09 -0500

    topbar.jsx

--- src/scenes_admin/global/topbar.jsx
+++ src/scenes_admin/global/topbar.jsx
@@ -1,5 +1,54 @@
-const Toolbar = ( ) => {
-    return <div>Toolbar</div>
-}
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificatnsOutlinedIcon from "@mui/icons-material/NotificatnsOutlined";
+import SettingOutlinedIcon from "@mui/icons-material/SettingOutlined";
+import PersnOutlinedIcon from "@mui/icons-material/PersnOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+ 
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificatnsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersnOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
 export default Toolbar;
